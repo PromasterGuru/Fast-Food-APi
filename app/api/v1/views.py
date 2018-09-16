4ec0b5c0b4a6c9e14e45fa8de392b9f0633b1eea
root <root@localhost.localdomain>
2018-09-16 22:24:34 +0300

    [Feature #160237860] Added API endpoint for placing an order

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -37,6 +37,28 @@ class Orders(Resource):
         response.status_code = 200 #OK
         return response
 
+    def post(self):
+        '''Place a new order'''
+        if not request.json or not "order_item" in request.json:
+            abort(400) #Bad Request
+        if not self.orders.get_orders():
+            order_id = 1
+        else:
+            order_id = self.orders.get_orders()[-1]['id'] + 1
+        new_order = {
+            "id": order_id,
+            "order_item": request.json['order_item'],
+            "description": request.json['description'],
+            "quantity": request.json['quantity'],
+    	    "order_date":str(datetime.datetime.now())[:19],
+            "status": "Pedding"
+        }
+        self.orders.get_orders().append(new_order)
+        result = {"Order": new_order}
+        response = jsonify(result)
+        response.status_code = 201 #Created
+        return response
+
 api.add_resource(Orders,'/api/v1/orders')
 
 if __name__ == '__main__':
