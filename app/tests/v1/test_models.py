62f1298f3ea56781f5d11dab16729f59daa2922a
root <root@localhost.localdomain>
2018-09-18 17:06:41 +0300

    [Fixes

--- app/tests/v1/test_models.py
+++ app/tests/v1/test_models.py
@@ -24,14 +24,20 @@ class TestModels(unittest.TestCase):
     def test_config(self):
         """Test configurations"""
         self.assertEqual(self.app.testing, True)
+        self.assertNotEqual(self.app.testing, False)
 
-    def test_isEmpty_init(self):
-        """Test the data structures are initially empty"""
+    def test_user_model_isEmpty_init(self):
+        """Test the user data structure is initially empty"""
         self.assertEqual(0,len(self.orders.get_orders()))
-        self.assertEqual(0,len(self.orders.get_users()))
+        self.assertNotEqual(1,len(self.orders.get_orders()))
 
-    def test_push1_record(self):
-        """Test when one record is added"""
+    def test_user_model_isEmpty_init(self):
+        """Test the food orders data structure is initially empty"""
+        self.assertEqual(0,len(self.orders.get_orders()))
+        self.assertNotEqual(1,len(self.orders.get_users()))
+
+    def test_push1_one_food_order_record(self):
+        """Test when one order record is added"""
         order = {
             "id": 1,
             "order_item": "Ugali",
@@ -42,7 +48,12 @@ class TestModels(unittest.TestCase):
         self.orders.set_orders(order)
         self.orders.set_users("paul","mathenge")
         self.assertEqual(1,len(self.orders.get_orders()))
+        self.assertNotEqual(0,len(self.orders.get_orders()))
+
+    def test_push1_one_user_record(self):
+        """Test when one user is registered in the database"""
         self.assertEqual(1,len(self.orders.get_users()))
+        self.assertNotEqual(0,len(self.orders.get_users()))
 
 if __name__ == '__main__':
     unittest.main()
